58d1d2095ed0fb3cd5278bb5f8f2d997ea49a8a3
Anderson Lima <anderson.sl93@hotmail.com>
2021-07-12 00:51:29 -0300

    add script

---
+++ README.md
@@ -0,0 +1,52 @@
+# Github Unwatch
+
+## Dependencies
+- [Python 3.6+](https://www.python.org/downloads/)
+
+## Configuration
+### Install
+
+```bash
+$ pip install requirements.txt
+```
+
+### Authentication token
+Generate a new token in [personal access tokens](https://github.com/settings/tokens) github page.
+
+## Run
+
+Using option:
+
+```bash
+$ python unwatch.py ORG --token <TOKEN>
+```
+
+Or using env var:
+
+```bash
+export GITHUB_TOKEN=<TOKEN>
+$ python unwatch.py ORG
+```
+
+## Safelist
+If exists some repositories you don't want unwatch, create a txt or json file named "safelist" and put in all repositories that should not be touched.
+
+Text file example:
+
+```txt
+repo1
+repo2
+repo3
+```
+
+Json file example:
+
+```json
+[
+    "repo1",
+    "repo2",
+    "repo3"
+]
+```
+
+*obs:* All safelists will be read, so if exists a `safelist.txt` and `safelist.json` the script will merge both ine one list to be avoided.
---
+++ unwatch.py
@@ -0,0 +1,60 @@
+import argparse
+import json
+import os
+
+# third imports
+import requests
+
+
+def read_safelist():
+    safelist = []
+    if os.path.exists('safelist.txt'):
+        with open('safelist.txt') as txt_file:
+            safelist.extend(repo.strip() for repo in txt_file.readlines())
+    if os.path.exists('safelist.json'):
+        with open('safelist.json') as json_file:
+            safelist.extend(repo.strip() for repo in json.load(json_file))
+    return set([repo for repo in safelist if repo])
+
+
+def repositories(org, token):
+    url = f'https://api.github.com/orgs/{org}/repos'
+    params = {'page': 1, 'per_page': 100}
+    headers = {'Authorization': f'token {token}'}
+    repos = []
+    while True:
+        res = requests.get(url, params=params, headers=headers)
+        page = res.json()
+        repos.extend(page)
+        if len(page) < 100:
+            break
+
+
+def unwatch_org(org, token):
+    repos = repositories(org, token)
+    safelist = read_safelist()
+    headers = {'Authorization': f'token {token}'}
+    for repo in repos:
+        if repo['name'] not in safelist:
+            res = requests.delete(repo['subscription_url'], headers=headers)
+            if res.status_code > 399:
+                print(f'failed to unwatch {repo["name"]}')
+                continue
+            print(f'successfully unwatch {repo["name"]}')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Unwatch/unsubscribe in all repositories of one '
+                    'organization ignoring the repositories in the safelist'
+    )
+    parser.add_argument('org', help='Organization')
+    parser.add_argument(
+        '--token',
+        default=os.environ.get('GITHUB_TOKEN'),
+        help='Authentication token'
+    )
+    args = parser.parse_args()
+    if args.token is None:
+        raise SystemExit('necessary the authentication token')
+    unwatch_org(args.org, args.token)
